70efc8fa70dc69868fa17b02d0d7c556b7523516
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-02 12:36:58 +0200

    MSB - Save data to localStorage

--- movieSeat/script.js
+++ movieSeat/script.js
@@ -6,9 +6,24 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+// Save selected movie index & price
+const setMovieData = (movieIndex, moviePrice) => {
+	localStorage.setItem("selectedMovieIndex", movieIndex);
+	localStorage.setItem("selectedMoviePrice", moviePrice);
+};
+
 // Update total & count
 const updateSelectedCount = () => {
 	const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+	// Copy selected seats into array
+	// Map through array
+	// Return new array of indexes
+	const seatsIndex = [ ...selectedSeats ].map((seat) => {
+		return [ ...seats ].indexOf(seat);
+	});
+
+	localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
 	count.innerText = selectedSeats.length;
 	total.innerText = selectedSeats.length * ticketPrice;
@@ -18,7 +33,7 @@ const updateSelectedCount = () => {
 movieSelect.addEventListener("change", (e) => {
 	e.preventDefault();
 	ticketPrice = +e.target.value;
-
+	setMovieData(e.target.selectedIndex, e.target.value);
 	updateSelectedCount();
 });
 
