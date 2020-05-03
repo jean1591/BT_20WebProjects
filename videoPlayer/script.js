bce588aa1aac18aa3e69549eeba0a70928569cea
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-03 11:53:26 +0200

    VP - JS

--- videoPlayer/script.js
+++ videoPlayer/script.js
@@ -24,12 +24,26 @@ const updatePlayIcon = () => {
 
 // Update progress & timestamp
 const updateProgress = () => {
-	return true;
+	progress.value = video.currentTime / video.duration * 100;
+
+	// Get minutes
+	let mins = Math.floor(video.currentTime / 60);
+	if (mins < 10) {
+		mins = `0${mins}`;
+	}
+
+	// Get seconds
+	let secs = Math.floor(video.currentTime % 60);
+	if (secs < 10) {
+		secs = `0${secs}`;
+	}
+
+	timestamp.innerHTML = `${mins}:${secs}`;
 };
 
 // Set video time to progress
 const setVideoProgress = () => {
-	return true;
+	video.currentTime = +progress.value * video.duration / 100;
 };
 
 // Stop video
@@ -46,4 +60,5 @@ video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 stop.addEventListener("click", stopVideo);
+
 progress.addEventListener("change", setVideoProgress);
