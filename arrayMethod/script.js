9992e15c6c0f8ae6ecfb7eac964870d49e6a4534
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-04 10:23:16 +0200

    AM - Add User

---
+++ arrayMethod/script.js
@@ -0,0 +1,57 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+// Fetch random user & add money
+const getRandomUser = async () => {
+	const res = await fetch("https://randomuser.me/api");
+	const data = await res.json();
+	const user = data.results[0];
+
+	// console.log(user);
+
+	const newUser = {
+		name: `${user.name.first} ${user.name.last}`,
+		nat: user.nat,
+		gender: user.gender,
+		money: Math.floor(Math.random() * 1000000)
+	};
+
+	addData(newUser);
+};
+
+// Add new object to data array
+const addData = (obj) => {
+	data.push(obj);
+
+	updateDom();
+};
+
+// Update DOM
+const updateDom = (providedData = data) => {
+	// Clear main div
+	main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+	providedData.forEach((item) => {
+		// Create new element & add class
+		const element = document.createElement("div");
+		element.classList.add("person");
+		// Construct element
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+		// Add to parent div
+		main.appendChild(element);
+	});
+};
+
+// Format number as money
+const formatMoney = (num) => {
+	return `$${num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}`;
+};
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
