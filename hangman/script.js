8637b194eb049b73f9d5e90981ec661f63fda08b
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-07 11:18:44 +0200

    HM - Final JS

--- hangman/script.js
+++ hangman/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById("word");
 const wrongLettersEl = document.getElementById("wrong-letters");
-const playAgainBtn = document.getElementById("play-again");
+const playAgainBtn = document.getElementById("play-button");
 const popup = document.getElementById("popup-container");
 const notification = document.getElementById("notification-container");
 const finalMessage = document.getElementById("final-message");
@@ -30,10 +30,88 @@ const displayWord = () => {
 
 	const innerWord = wordEl.innerText.replace(/\n/g, "");
 
+	// Check if player has won
 	if (innerWord === selectedWord) {
 		finalMessage.innerText = "Congratulations! You won! 😃";
 		popup.style.display = "flex";
 	}
 };
+
+// Update the wrong letter
+const updateWrongLettersEl = () => {
+	// Display wrong letters
+	wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${letter}</span>`)}
+  `;
+
+	// Display parts
+	figureParts.forEach((part, index) => {
+		const errors = wrongLetters.length;
+		if (index < errors) {
+			part.style.display = "block";
+		} else {
+			part.style.display = "none";
+		}
+	});
+
+	// Check if lost
+	if (wrongLetters.length === figureParts.length) {
+		finalMessage.innerText = "LoOoOoSeEeR !!!! 😕";
+		popup.style.display = "flex";
+	}
+};
+
+// Show notification
+const showNotification = () => {
+	notification.classList.add("show");
+
+	setTimeout(() => {
+		notification.classList.remove("show");
+	}, 2000);
+};
+
+// Keydown letter press
+window.addEventListener("keydown", (e) => {
+	if (e.keyCode >= 65 && e.keyCode <= 90) {
+		const letter = e.key;
+
+		if (selectedWord.includes(letter)) {
+			if (!correctLetters.includes(letter)) {
+				// Letter is in selectedWord and not typed yet
+				correctLetters.push(letter);
+				displayWord();
+			} else {
+				// Letter is in selectedWord and already typed
+				showNotification();
+			}
+		} else {
+			// Letter is not in selectedWord
+			if (!wrongLetters.includes(letter)) {
+				wrongLetters.push(letter);
+				updateWrongLettersEl();
+			} else {
+				// Letter is not in selectedWord and already typed
+				showNotification();
+			}
+		}
+	}
+});
+
+// Restart game and play again
+playAgainBtn.addEventListener("click", () => {
+	// Empty arrays
+	correctLetters.splice(0);
+	wrongLetters.splice(0);
+
+	// Get new word
+	selectedWord = words[Math.floor(Math.random() * words.length)];
+
+	displayWord();
+
+	// Delete figure parts and popup
+	updateWrongLettersEl();
+	popup.style.display = "none";
+});
 
 displayWord();
