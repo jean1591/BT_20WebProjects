3b9de73c0491be36b34f92aca994c18fcc0657ba
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-10 15:05:18 +0200

    MF - js

--- mealFinder/script.js
+++ mealFinder/script.js
@@ -45,5 +45,63 @@ const searchMeal = async (e) => {
 	}
 };
 
+// Fetch meal by id
+const getMealById = async (mealId) => {
+	const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+	const data = await res.json();
+	console.log(data);
+
+	const meal = data.meals[0];
+	addMealToDOM(meal);
+};
+
+// Add meal to DOM
+const addMealToDOM = (meal) => {
+	const ingredients = [];
+
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(`${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`);
+		} else {
+			break;
+		}
+	}
+
+	single_mealsEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstruction}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+        </ul>
+      </div>
+    </div>
+  `;
+
+	console.log(ingredients);
+};
+
 // Event listeners
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+	const mealInfo = e.path.find((item) => {
+		if (item.classList) {
+			return item.classList.contains("meal-info");
+		} else {
+			return false;
+		}
+	});
+
+	if (mealInfo) {
+		const mealId = mealInfo.getAttribute("data-mealid");
+		getMealById(mealId);
+	}
+});
