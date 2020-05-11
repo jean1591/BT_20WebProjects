44c7601195e99bb5fda5034c78ea5a9650e04844
Jean Robertou <jean.robertou@whereyoulove.com>
2020-05-11 10:22:37 +0200

    ET - Update amounts

--- expenseTracker/script.js
+++ expenseTracker/script.js
@@ -35,11 +35,30 @@ const addTransactionDOM = (transaction) => {
 	list.appendChild(item);
 };
 
+// Update balance, income & expense
+const updateValues = () => {
+	const amounts = transactions.map((transaction) => transaction.amount);
+
+	const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+	const income = amounts
+		.filter((item) => item > 0)
+		.reduce((acc, item) => (acc += item), 0)
+		.toFixed(2);
+	const expense =
+		amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) * -(1).toFixed(2);
+
+	balance.innerText = `$${total}`;
+	moneyPlus.innerText = `$${income}`;
+	moneyMinus.innerText = `$${expense}`;
+};
+
 // Init App
 const init = () => {
 	list.innerHTML = "";
-	// transactions.forEach((t) => addTransactionDOM(t));
-	transactions.forEach(addTransactionDOM);
+	transactions.forEach((t) => addTransactionDOM(t));
+
+	updateValues();
 };
 
 init();
